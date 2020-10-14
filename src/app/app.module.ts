c098e2eb0d5ac8ec4d4ec6b95e786df9b289d373
Marius Chelariu <chelariu_marius90@yahoo.com>
2020-10-15 00:41:27 +0300

    Add timepicker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { TimepickerComponent } from '../control/timepicker/timepicker.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimepickerComponent
   ],
   imports: [
     BrowserModule,
